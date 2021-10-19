1dfabf6ad10bfc8da8b813696bc28ec6a2f63625
GaillA <anthomee@hotmail.fr>
2021-10-19 10:58:06 +0200

    fix useless use

--- src/Controller/PizzaController.php
+++ src/Controller/PizzaController.php
@@ -9,7 +9,6 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Entity\Pizza;
-use Symfony\Component\VarDumper\VarDumper;
 
 class PizzaController extends AbstractController
 {
